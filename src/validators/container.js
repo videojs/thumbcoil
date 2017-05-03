8c62d499b2ab72c1e8d28c259dbd3539d2976af8
Garrett Singer <gesinger@gmail.com>
2017-05-03 10:46:17 -0400

    Add container checks for frame(s) with unknown type(s), more than one audio track, packets with PID 0 but not a non-PAT type, and more than one program in stream.

--- src/validators/container.js
+++ src/validators/container.js
@@ -37,6 +37,48 @@ export const validateContainers = (esMap) => {
     warnings.push('Video has no I frames');
   }
 
+  const unknownFrames = esMap.filter((esEl) => esEl.type.startsWith('unknown-'));
+
+  if (unknownFrames.length > 0) {
+    // TODO determine if there are any unsupported codecs
+    warnings.push(`Detected ${unknownFrames.length} frames with unknown types`);
+  }
+
+  const pid0Packets = esMap.filter((esEl) => esEl.pid === 0);
+  const invalidPid0Packets = pid0Packets.filter((esEl) => esEl.type !== 'pat');
+
+  if (invalidPid0Packets.length > 0) {
+    errors.push(
+      `Detected ${invalidPid0Packets.length} packets with pid 0 and a non-PAT type`);
+  }
+
+  // we only parse the first PMT PID from the PAT, but we can check for extra programs
+  // by seeing if there are PMTs with different IDs
+
+  const pmtPackets = esMap.filter((esEl) => esEl.type === 'pmt');
+  const pmtPids = Array.from(new Set(pmtPackets.map((packet) => packet.pid)));
+
+  if (pmtPids.length > 1) {
+    errors.push(
+      `Detected ${pmtPids.length} programs in the stream (more than allowed 1)`);
+  }
+
+  let tracks = [];
+
+  pmtPackets.forEach((pmtPacket) => {
+    tracks = tracks.concat(pmtPacket.tracks);
+  });
+
+  const audioTracks = tracks.filter((track) => track.type === 'audio');
+
+  if (audioTracks.length > 1) {
+    warnings.push(`Detected ${audioTracks.length} audio tracks (more than preferred 1)`);
+  }
+
+  // TODO
+  // WARNINGS
+  // unsupported codecs
+
   return {
     warnings,
     errors
